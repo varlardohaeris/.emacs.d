fb6cd18e6693d7c98c7f5e7c6c6ed6cb17d599ab
wujiahua <a_wujiahua@126.com>
2020-03-12 00:49:36 +0800

    Set dracula theme and Jetbrains mono

---
+++ lisp/init-base.el
@@ -0,0 +1,3 @@
+(add-to-list 'exec-path "/usr/local/bin")
+(set-keyboard-coding-system nil)
+(provide 'init-base)
---
+++ lisp/init-theme.el
@@ -0,0 +1,7 @@
+(add-to-list 'default-frame-alist
+	     '(font . "JetBrains Mono-16"))
+
+(require-package 'dracula-theme)
+(load-theme 'dracula t)
+
+(provide 'init-theme)
