94241387a8ae24b466b1d23f85fc6700214a641a
wujiahua <a_wujiahua@126.com>
2020-04-12 09:59:17 +0800

    Configure ctags for etags-pakcage

--- lisp/init-counsel-etags.el
+++ lisp/init-counsel-etags.el
@@ -13,5 +13,11 @@
   (setq counsel-etags-update-interval 60)
   (push "build" counsel-etags-ignore-directories))
 
+(when *is-a-mac*
+  ;; Mac's default ctags does not support -e option
+  ;; If you install Emacs by homebrew, another version of etags is already installed which does not need -e too
+  ;; the best option is to install latest ctags from sf.net
+  (setq ctags-command "/usr/local/bin/ctags -e -R "))
+
 (provide 'init-counsel-etags)
 ;;; init-counsel-etags.el ends here
