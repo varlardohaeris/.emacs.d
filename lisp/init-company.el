e8929e23e4e2018c2ffae49f734ca43d6ab19d3c
wujiahua <a_wujiahua@126.com>
2020-04-23 00:44:31 +0800

    remap ctrl-n  ctrl-p to company-select-next, company-selecct-prev

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -7,5 +7,11 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-company)
 ;;; init-company.el ends here
