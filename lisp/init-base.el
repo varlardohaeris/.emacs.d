ef2812d97ffea2024cb07151e7543eb259cc588e
wujiahua <a_wujiahua@126.com>
2020-09-26 21:13:57 +0800

    set proxy on mac os

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -20,15 +20,19 @@
 (setq create-lockfiles nil)
 (setq company-dabbrev-downcase nil)
 
-(setq url-proxy-services
-      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-	("http" . "127.0.0.1:7890")
-	("https" . "127.0.0.1:7890")))
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+(when *is-a-mac*
+  (setq url-proxy-services
+	'(("no_proxy" . "^\\(localhost\\|10.*\\)")
+	  ("http" . "127.0.0.1:7890")
+	  ("https" . "127.0.0.1:7890"))))
+
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
-(defconst *is-a-mac* (eq system-type 'darwin))
 
 (add-to-list 'default-frame-alist
 	     '(font . "JetBrains Mono Medium-18"))
