f02d1000122034aa9d6a19262890995a542e14f6
wujiahua <a_wujiahua@126.com>
2020-10-14 21:43:33 +0800

    delete org icons

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -5,7 +5,6 @@
 (use-package org-bullets
   :ensure t
   :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-  (setq org-bullets-bullet-list '("☰" "☷" "✿" "☭")))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 (provide 'init-org)
 ;;; init-org.el ends here
