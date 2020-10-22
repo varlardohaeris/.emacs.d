905188bd63b8667c337a150dbd8dfdfec375fde7
wujiahua <a_wujiahua@126.com>
2020-10-22 23:31:27 +0800

    enable evil-leader before evil-mode

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -2,41 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package evil
-  :ensure t
-  :config
-
-  (evil-mode 1)
-
-  ;; config evil
-  (setcdr evil-insert-state-map nil)
-  (define-key evil-insert-state-map [escape] 'evil-normal-state)
-
-  ;; j/k for browsing wrapped lines
-  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-
-
-  ;; page up with Ctrl-k
-  (define-key evil-normal-state-map (kbd "C-k") (lambda()
-						  (interactive)
-						  (evil-scroll-up nil)))
-
-  ;; page down with Ctrl-j
-  (define-key evil-normal-state-map (kbd "C-j") (lambda()
-						  (interactive)
-						  (evil-scroll-down nil)))
-
-  (define-key evil-insert-state-map (kbd "C-c C-c") (lambda()
-						      (interactive)
-						      (evil-normal-state)))
-
-
-  (define-key evil-insert-state-map (kbd "C-c C-c") (lambda()
-						      (interactive)
-						      (evil-normal-state))))
-
-
 (use-package evil-leader
   :commands (evil-leader-mode)
   :ensure evil-leader
@@ -79,6 +44,40 @@
       "wj"  'windmove-down
       "wd"  'delete-other-windows)))
 
+
+(use-package evil
+  :ensure t
+  :config
+
+  (evil-mode 1)
+
+  ;; config evil
+  (setcdr evil-insert-state-map nil)
+  (define-key evil-insert-state-map [escape] 'evil-normal-state)
+
+  ;; j/k for browsing wrapped lines
+  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+
+
+  ;; page up with Ctrl-k
+  (define-key evil-normal-state-map (kbd "C-k") (lambda()
+						  (interactive)
+						  (evil-scroll-up nil)))
+
+  ;; page down with Ctrl-j
+  (define-key evil-normal-state-map (kbd "C-j") (lambda()
+						  (interactive)
+						  (evil-scroll-down nil)))
+
+  (define-key evil-insert-state-map (kbd "C-c C-c") (lambda()
+						      (interactive)
+						      (evil-normal-state)))
+
+
+  (define-key evil-insert-state-map (kbd "C-c C-c") (lambda()
+						      (interactive)
+						      (evil-normal-state))))
 (use-package evil-matchit
   :ensure t
   :config
