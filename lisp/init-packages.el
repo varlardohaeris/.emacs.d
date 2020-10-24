d7a894b347c78785a86d49cda0994571562136b0
wujiahua <a_wujiahua@126.com>
2020-10-24 23:21:52 +0800

    install auto-package-update by default

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -41,6 +41,7 @@
 (setq use-package-hook-name-suffix nil)
 
 (use-package auto-package-update
+  :ensure t
   :config
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
