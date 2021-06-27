fb70b213e6ace9e8979b08fa975747b99a4125c3
wujiahua <wujiahua_lisp@outlook.com>
2021-06-27 11:37:44 +0800

    Enable company globally

--- init.el
+++ init.el
@@ -347,12 +347,13 @@
   (setq typescript-indent-level 2))
 
 (use-package company
-  :after lsp-mode
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
   :hook (lsp-mode . company-mode)
   :bind (:map company-active-map
          ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
+        ;; (:map lsp-mode-map
+        ;;  ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
