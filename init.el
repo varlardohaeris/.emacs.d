959630711e53715a070647f92582f37c07f3a5a2
wujiahua <wujiahua_lisp@outlook.com>
2021-06-27 09:40:37 +0800

    auto update emacs packages

--- init.el
+++ init.el
@@ -377,3 +377,13 @@
 
 (use-package all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
+
+
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
