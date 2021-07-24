bd394c96d30528b1c42330bc22c4722a5f6c8bf1
wujiahua <wujiahua_lisp@outlook.com>
2021-07-24 19:57:51 +0800

    disable org-temp

--- init.el
+++ init.el
@@ -309,10 +309,10 @@
  '((emacs-lisp . t)
    (python . t)))
 
-(require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
+;; (require 'org-tempo)
+;; (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+;; (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+;; (add-to-list 'org-structure-template-alist '("py" . "src python"))
 
 
 ;; (use-package smartparens
